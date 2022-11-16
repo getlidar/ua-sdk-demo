e258a94b57a18b324f58fabbc1e4817264089221
Christian Sumido <gcbsumid@gmail.com>
2022-11-16 15:49:49 -0800

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import './App.css';
+import { useState } from 'react';
+
 function App() {
+
+  const [wallet,setWallet] = useState('');
 
   async function mint(){
     // todo: Log referral here
+    console.log(wallet)
   }
 
   return (
-      <div className="App">
-        <div>
-          Input Wallet Address
-        </div>
-        <input
-          type={'text'}
-          placeholder={'0x...'}
-        />
-        <button
-          onClick={()=> mint}
-        >
-          MINT
-        </button>
-      
+    <div className="App">
+      <div>
+        Input Wallet Address
       </div>
+      <input
+        type={'text'}
+        placeholder={'0x...'}
+        onChange={(e) => {
+          setWallet(e.target.value.trim());
+        }}
+      />
+      <button
+        onClick={()=> mint()}
+      >
+        MINT
+      </button>
+    
+    </div>
   );
 }
 
