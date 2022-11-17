0c3687ed2b68a911986aba5a4f9cdfecf1207255
Christian Sumido <gcbsumid@gmail.com>
2022-11-16 16:19:50 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import { useState } from 'react';
+import Refmint from "refmint-sdk";
+import {useLocation} from "react-router-dom";
 
 function App() {
 
   const [wallet,setWallet] = useState('');
 
+  const search = useLocation().search;
+  const referral_link = new URLSearchParams(search).get('r');
+
+  var refmintClient = new Refmint({
+    apiKey: process.env.REACT_APP_API_KEY,
+    baseUrl: "https://test.refmint.xyz"
+  });
+
   async function mint(){
-    // todo: Log referral here    
+    // logReferral( custom_url, wallet_address, link_id, email_address, phone_number)
+    refmintClient.logReferral("demo", wallet, referral_link, null, null)
+        .then(resp => {
+            console.log(resp);
+
+            //! initiate NFT mint here
+        }
+    );
+
     console.log(wallet);
   }
 
