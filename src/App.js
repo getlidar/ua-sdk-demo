98c8040ae6edddcab529f1d11d95c8c1e759600c
gpbsumido <gpbsumido@gmail.com>
2023-02-08 10:01:04 -0800

    update to v18

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import './App.css';
 import { useState } from 'react';
 import Refmint from "refmint-sdk";
+import { BaseURLOptions } from 'refmint-sdk';
 import {useLocation} from "react-router-dom";
 
 function App() {
+
+  let urlOption;
+  const base_url = 'https://test.refmint.xyz';
+
+  if (base_url === 'http://localhost:3000') {
+    urlOption = BaseURLOptions.LOCAL;
+  } else if (base_url === 'https://test.refmint.xyz') {
+    urlOption = BaseURLOptions.TESTNET;
+  } else if (base_url === 'https://app.refmint.xyz') {
+    urlOption = BaseURLOptions.MAINNET;
+  }
+
+  var refmintCaller = new Refmint({
+    apiKey: process.env.REACT_APP_API_KEY,
+    baseUrlOption: urlOption
+  });
 
   const [wallet,setWallet] = useState('');
 
   const search = useLocation().search;
   const referral_link = new URLSearchParams(search).get('r');
 
-  var refmintClient = new Refmint({
-    apiKey: process.env.REACT_APP_API_KEY,
-    baseUrl: "https://test.refmint.xyz"
-  });
+  async function logReferral() {
+    
+    refmintCaller.logReferral(
+      'refmintsdk', //example custom url of project on testnet
+      wallet, // example wallet address
+      referral_link, // example link id for user on testnet // optional
+      // email_address, // email address of refeee // optional
+      // phone_number // phone number of refeee // optional
+    ).then((resp) => {
 
-  async function mint(){
-    // logReferral( custom_url, wallet_address, link_id, email_address, phone_number)
-    refmintClient.logReferral("demo", wallet, referral_link, null, null)
-        .then(resp => {
-            console.log(resp);
+      console.log(resp);
+      //! initiate NFT mint here
 
-            //! initiate NFT mint here
-        }
-    );
-
-    console.log(wallet);
+    }).catch(e => {
+      console.error(e);
+    });
   }
 
   return (
@@ -41,7 +58,7 @@ function App() {
         }}
       />
       <button
-        onClick={()=> mint()}
+        onClick={()=> logReferral()}
       >
         MINT
       </button>
