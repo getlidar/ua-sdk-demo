14ce808d6de5f231b1f316b5f05e38442501003a
gpbsumido <gpbsumido@gmail.com>
2023-02-08 09:42:47 -0800

    updated to v.1.1.18 of refmint sdk

--- src/components/ApiForm.tsx
+++ src/components/ApiForm.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import { useState } from "react";
 import Refmint from "refmint-sdk";
 import { InputSection } from "./InputSection";
+import { useLocation } from "react-router-dom";
+import { BaseURLOptions } from 'refmint-sdk';
 
 export function ApiForm() {
 
+  const search = useLocation().search;
+  const new_referral_link = new URLSearchParams(search).get('r');
+
   async function logReferral() {
+
+    let urlOption;
+
+    if (base_url === 'http://localhost:3000') {
+      urlOption = BaseURLOptions.LOCAL;
+    } else if (base_url === 'https://test.refmint.xyz') {
+      urlOption = BaseURLOptions.TESTNET;
+    } else if (base_url === 'https://app.refmint.xyz') {
+      urlOption = BaseURLOptions.MAINNET;
+    } else {
+      set_sdk_response('INVALID BASE_URL');
+    }
 
     var refmintCaller = new Refmint({
       apiKey: api_key,
-      baseUrl: (base_url.trim() !== '' ? base_url : "https://test.refmint.xyz")
+      baseUrlOption: urlOption
     });
     
     refmintCaller.logReferral(
@@ -27,14 +44,76 @@ export function ApiForm() {
     });
   }
 
-  const [custom_url,set_custom_url] = useState<string>('');
+  async function logView() {
+
+    let urlOption;
+
+    if (base_url === 'http://localhost:3000') {
+      urlOption = BaseURLOptions.LOCAL;
+    } else if (base_url === 'https://test.refmint.xyz') {
+      urlOption = BaseURLOptions.TESTNET;
+    } else if (base_url === 'https://app.refmint.xyz') {
+      urlOption = BaseURLOptions.MAINNET;
+    } else {
+      set_sdk_response('INVALID BASE_URL');
+    }
+
+    var refmintCaller = new Refmint({
+      apiKey: api_key,
+      baseUrlOption: urlOption
+    });
+    
+    refmintCaller.logView(
+      custom_url,
+      link_id
+    ).then((resp) => {
+      if (!resp) set_sdk_response('API failed.')
+      set_sdk_response(JSON.stringify(resp));
+    }).catch(e => {
+      console.log(e);
+      set_sdk_response(e.message)
+    });
+  }
+
+  async function isAffiliate() {
+
+    let urlOption;
+
+    if (base_url === 'http://localhost:3000') {
+      urlOption = BaseURLOptions.LOCAL;
+    } else if (base_url === 'https://test.refmint.xyz') {
+      urlOption = BaseURLOptions.TESTNET;
+    } else if (base_url === 'https://app.refmint.xyz') {
+      urlOption = BaseURLOptions.MAINNET;
+    } else {
+      set_sdk_response('INVALID BASE_URL');
+    }
+
+    var refmintCaller = new Refmint({
+      apiKey: api_key,
+      baseUrlOption: urlOption
+    });
+    
+    refmintCaller.isAffiliate(
+      custom_url,
+      wallet_address
+    ).then((resp) => {
+      if (!resp) set_sdk_response('API failed.')
+      set_sdk_response(JSON.stringify(resp));
+    }).catch(e => {
+      console.log(e);
+      set_sdk_response(e.message)
+    });
+  }
+
+  const [custom_url,set_custom_url] = useState<string>('refmintsdk');
   const [wallet_address,set_wallet_address] = useState<string>('');
-  const [link_id,set_link_id] = useState<string>('');
+  const [link_id,set_link_id] = useState<string>(new_referral_link ? new_referral_link : 'fqOm45Jv');
   const [email_address,set_email_address] = useState<string>('');
   const [phone_number,set_phone_number] = useState<string>('');
   const [sdk_response,set_sdk_response] = useState<string>('');
-  const [base_url,set_base_url] = useState<string>('');
-  const [api_key,set_api_key] = useState<string>('');
+  const [base_url,set_base_url] = useState<string>('https://test.refmint.xyz');
+  const [api_key,set_api_key] = useState<string>('kLJfpVvWZ0ERxnMofhP9iRJTxwDapndo8941KuxK76XOOvsyMVsXjihnRTA0R06y');
 
   return(
     <div className='mx-auto flex flex-col max-w-[calc(min(50%,1200px))]'>
@@ -108,12 +187,26 @@ export function ApiForm() {
         isTextField={false}
       />
       </div>
-      <button
-        className='w-min mx-auto px-3 mt-5 bg-referralMintColor rounded-lg'
-        onClick={()=>logReferral()}
-      >
-        Submit
-      </button>
+      <div className='flex flex-row space-x-3 mx-auto'>
+        <button
+          className='w-min px-3 mt-5 bg-referralMintColor rounded-lg whitespace-nowrap'
+          onClick={()=>logView()}
+        >
+          LogView
+        </button>
+        <button
+          className='w-min px-3 mt-5 bg-referralMintColor rounded-lg whitespace-nowrap'
+          onClick={()=>logReferral()}
+        >
+          Log Referral
+        </button>
+        <button
+          className='w-min px-3 mt-5 bg-referralMintColor rounded-lg whitespace-nowrap'
+          onClick={()=>isAffiliate()}
+        >
+          IsAffiliate
+        </button>
+        </div>
       <div className="w-full flex flex-row justify-center">
         <InputSection
           title='Response'
